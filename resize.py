9da8ca14d882ffe130394a9d5f28bd74403f98b7
TedLee <ted1050217@gmail.com>
2024-11-26 16:58:57 +0800

    change: speed up by numba.jit

--- resize.py
+++ resize.py
@@ -1,9 +1,12 @@
 import numpy as np
+from numba import jit
 from scipy import signal
 import cv2
 import matplotlib.pyplot as plt
+import time
 
 # Remove a seam from img
+@jit
 def remove(img: np.ndarray, seam) -> np.ndarray:
     h, w, c = img.shape
     ret_img = np.zeros((h, w-1, 3))
@@ -12,6 +15,38 @@ def remove(img: np.ndarray, seam) -> np.ndarray:
         ret_img[i, :m] = img[i, :m]
         ret_img[i, m:] = img[i, m+1:]
     return ret_img 
+
+@jit 
+def find_seam(energy_map: np.ndarray) -> np.ndarray:
+    h, w = energy_map.shape
+    dp = np.zeros((h, w))
+
+    dp[0, :] = energy_map[0, :]
+    for i in range(1, h):
+        for j in range(0, w):
+            if(j == 0):
+                dp[i, j] = min(dp[i-1, j], dp[i-1, j+1]) + energy_map[i, j]
+            elif(j==w-1):
+                dp[i, j] = min(dp[i-1, j-1], dp[i-1, j]) + energy_map[i, j]
+            else:
+                dp[i, j] = min(dp[i-1, j-1], min(dp[i-1, j], dp[i-1, j+1])) + energy_map[i, j]
+
+    y_s = np.argmin(dp[h-1, :])
+    seam = np.zeros((h, 2), dtype=np.int32)
+    seam[-1] = [h-1, y_s]
+    for i in range(h-2, -1, -1):
+        x, y = seam[-1]
+        # print(f"(x,y) = {x, y}")
+        if(dp[x, y] == dp[x-1, y-1] + energy_map[x, y]):
+            seam[i] = [x-1, y-1]
+        elif(dp[x, y] == dp[x-1, y] + energy_map[x, y]):
+            seam[i] = [x-1, y-1]
+        elif(dp[x, y] == dp[x-1, y+1] + energy_map[x, y]):
+            seam[i] = [x-1, y-1]
+        else:
+            print("Something wrong when compute dp")
+            assert(1 == 0)
+    return seam
 
 
 def delete_vertical(img: np.ndarray):
@@ -37,43 +72,47 @@ def delete_vertical(img: np.ndarray):
 
     energy_map = np.abs(convolved_dx) + np.abs(convolved_dy)
 
-    dp = np.zeros((h, w))
+    # dp = np.zeros((h, w))
 
-    dp[0, :] = energy_map[0, :]
-    for i in range(1, h):
-        for j in range(0, w):
-            if(j == 0):
-                dp[i, j] = min(dp[i-1, j], dp[i-1, j+1]) + energy_map[i, j]
-            elif(j==w-1):
-                dp[i, j] = min(dp[i-1, j-1], dp[i-1, j]) + energy_map[i, j]
-            else:
-                dp[i, j] = min(dp[i-1, j-1], min(dp[i-1, j], dp[i-1, j+1])) + energy_map[i, j]
+    # dp[0, :] = energy_map[0, :]
+    # for i in range(1, h):
+    #     for j in range(0, w):
+    #         if(j == 0):
+    #             dp[i, j] = min(dp[i-1, j], dp[i-1, j+1]) + energy_map[i, j]
+    #         elif(j==w-1):
+    #             dp[i, j] = min(dp[i-1, j-1], dp[i-1, j]) + energy_map[i, j]
+    #         else:
+    #             dp[i, j] = min(dp[i-1, j-1], min(dp[i-1, j], dp[i-1, j+1])) + energy_map[i, j]
 
-    y_s = np.argmin(dp[h-1, :])
-    seam = []
-    seam.append([h-1, y_s])
-    for i in range(h-2, -1, -1):
-        x, y = seam[-1]
-        # print(f"(x,y) = {x, y}")
-        if(dp[x, y] == dp[x-1, y-1] + energy_map[x, y]):
-            seam.append([x-1, y-1])
-        elif(dp[x, y] == dp[x-1, y] + energy_map[x, y]):
-            seam.append([x-1, y])
-        elif(dp[x, y] == dp[x-1, y+1] + energy_map[x, y]):
-            seam.append([x-1, y+1])
-        else:
-            print("Something wrong when compute dp")
-            assert(1 == 0)
-    return remove(img=img, seam=seam)
+    # y_s = np.argmin(dp[h-1, :])
+    # seam = np.zeros((h, 2), dtype=np.int32)
+    # seam[-1] = [h-1, y_s]
+    # for i in range(h-2, -1, -1):
+    #     x, y = seam[-1]
+    #     # print(f"(x,y) = {x, y}")
+    #     if(dp[x, y] == dp[x-1, y-1] + energy_map[x, y]):
+    #         seam[i] = [x-1, y-1]
+    #     elif(dp[x, y] == dp[x-1, y] + energy_map[x, y]):
+    #         seam[i] = [x-1, y-1]
+    #     elif(dp[x, y] == dp[x-1, y+1] + energy_map[x, y]):
+    #         seam[i] = [x-1, y-1]
+    #     else:
+    #         print("Something wrong when compute dp")
+    #         assert(1 == 0)
+    seam = find_seam(energy_map=energy_map)
+    ret = remove(img=img, seam=seam)
+    return ret
 
 def main():
+    start_time = time.time()
     img = cv2.imread('image/cat.jpg')
-    numOfDelete = 100
+    numOfDelete = 10
     origin_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = origin_img
     for i in range(numOfDelete):
         # print(f"img[50, 10:20] = {img[50, 10:20]}")
         img = delete_vertical(img)
+    print('Time used: {} sec'.format(time.time()-start_time))
     img = np.int32(img)
     fig, axes = plt.subplots(2, 1, figsize = (9, 10))
     axes[0].imshow(origin_img)
